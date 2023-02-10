b0644ddfa2d39875cbd7f17299f4c5ee95611b9d
LiL <90929523+milkfirst@users.noreply.github.com>
2023-02-10 09:38:38 -0500

    adding notes on js and updating read me

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 +console.log('Are you ready to drop some dinos? :D');
 
+/* use the querySelectorAll method to select
+all HTML elements with class "dino" and class "drop-zone" */
+
 const dinos = document.querySelectorAll(".dino");
 const dropZones = document.querySelectorAll(".drop-zone");
+
+/* loop through the dinos array using the forEach method
+and set a unique id for each dinosaur element and adds drag and drop event listeners to each dinosaur.  */
 
 dinos.forEach((dino, index) => {
   dino.setAttribute("id", `dino-${index}`);
@@ -16,6 +22,8 @@ dropZones.forEach(dropZone => {
   dropZone.addEventListener("drop", dropped);
 });
 
+/* dragstart event listener is triggered when a user starts to drag a dinosaur */
+
 function dragStart(event) {
     event.dataTransfer.setData("text/plain", event.target.id);
   
@@ -26,7 +34,9 @@ function dragStart(event) {
     event.target.style.width = originalSize + "80px";
     event.target.style.height = originalSize + "80px";
 }
-  
+
+/* loop through the dropZones array and adds drag and drop event 
+listeners to each drop zone */
 
 function dragEnd(e) {
   const dino = e.target;
@@ -37,21 +47,29 @@ function dragEnd(e) {
   console.log('dropped this dino on me:', this);
 }
 
+/*  dragenter event listener is triggered when a dinosaur enters a drop zone */
+
 function dragEnter(e) {
   e.preventDefault();
   const dropZone = e.target;
   dropZone.style.backgroundColor = "rgba(0, 255, 0, 0.2)";
 }
 
+/* dragleave event listener is triggered when a dinosaur leaves a drop zone */
+
 function dragLeave(e) {
   const dropZone = e.target;
   dropZone.style.backgroundColor = "inherit";
 }
 
+/* dragover event listener is triggered when a dinosaur is being dragged over a drop zone and it logs a message indicating the time in milliseconds (or i don't know what the clicks are) that the dinosaur is being dragged over the drop zone. */
+
 function dragOver(e) {
   e.preventDefault();
   console.log('is how many milliseconds dino is being dragged over me'); 
 }
+
+/* the drop event listener is triggered when a dinosaur is dropped on a drop zone and it appends the dinosaur to the drop zone. */
 
 function dropped(e) {
   e.preventDefault();
@@ -63,4 +81,3 @@ function dropped(e) {
   dropZone.appendChild(dino);
   console.log('the dino has been dropped over me');
 }
-
